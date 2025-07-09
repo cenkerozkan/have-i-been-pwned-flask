5e7b5447b6a7ed6cd0152422c23bedac8e2ab069
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2025-07-10 00:23:43 +0300

    Update singleton.py with an example.

--- decorators/sinlgeton.py
+++ decorators/sinlgeton.py
@@ -1,7 +1,18 @@
 def singleton(class_):
-    instances = {}
+    instances: dict = {}
     def getinstance(*args, **kwargs):
         if class_ not in instances:
             instances[class_] = class_(*args, **kwargs)
         return instances[class_]
     return getinstance
+
+
+if __name__ == "__main__":
+    # To see if it works.
+    @singleton
+    class MyClass:
+        pass
+
+    obj1 = MyClass()
+    obj2 = MyClass()
+    print(obj1 is obj2)
