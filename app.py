8330d507a85471cf87797fb894b36dffb549f7f5
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-07-10 03:59:51 +0300

    Created File Tree

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from markupsafe import escape
+from util.logger import get_logger
+
+logger = get_logger(__name__)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
+
+@app.route("/<name>")
+def hello_name(name):
+    logger.info(f"Hello {name}")
+    return escape(name)
