a955b33b311341a1616982c9beb72b7412940901
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-08-08 01:18:28 +0300

    refactor: New title column in pwned_platform.py

--- db/model/pwned_platform.py
+++ db/model/pwned_platform.py
@@ -8,8 +8,9 @@ class PwnedPlatform(db.Model):
     # Columns
     id = db.Column(db.Integer, primary_key=True)
     email_id = db.Column(db.Integer, db.ForeignKey("emails.id"), nullable=False)
+    title = db.Column(db.String, nullable=True)
     domain = db.Column(db.String, nullable=False)
-    breach_date = db.Column(db.String)
+    breach_date = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, nullable=True, default=datetime.now())
     descripiton = db.Column(db.String, nullable=True)
     is_verified = db.Column(db.Boolean, nullable=False, default=False)
