bbe931f0689be4cf73db8d3afadf31d842eeb224
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-08-19 14:41:38 +0300

    Add PwnChecker class for email breach checking functionality

---
+++ task/pwn_checker.py
@@ -0,0 +1,17 @@
+from util.have_i_been_pwned_request_executor import HaveIBeenPwnedRequestExecutor
+from repository.email_repository import EmailRepository
+from repository.pwned_platform_repository import PwnedPlatformRepository
+
+from db.model.email import Email
+
+class PwnChecker:
+    def __init__(self):
+        self._pwned_request_executor = HaveIBeenPwnedRequestExecutor()
+        self._email_repository = EmailRepository()
+        self._pwned_platform_repository = PwnedPlatformRepository()
+
+    def _get_all_emails(self):
+        emails: list[Email] = self._email_repository.get_all()
+
+    def run(self):
+        pass
